63004b8ef8de6455fbb95a43f25d24801997ff70
M Akmal F <makmalfath@gmail.com>
2024-07-18 10:38:41 +0700

    Setup e-commerce API file and folder using Bun and Express

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import express from "express";
+
+const app = express();
+const APP_PORT = 3000;
+
+app.get("/", (req: express.Request, res: express.Response) => {
+  res.send("Hello World!");
+});
+
+app.listen(APP_PORT, () => {
+  console.log(`Server listening on port ${APP_PORT}`);
+});
